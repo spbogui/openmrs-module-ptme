b6693efce4513addf24785b476ee13badc79aac7
spbogui <spbogui@gmail.com>
2018-10-23 22:45:43 +0000

    Form to get DataSet fom

---
+++ api/src/main/java/org/openmrs/module/ptme/forms/GetDataSetFromFrom.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.ptme.forms;
+
+public class GetDataSetFromFrom {
+    private String mode;
+
+    public GetDataSetFromFrom() {
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+}
