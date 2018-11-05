cbfb436ef6473446f0a4c7500422355ff1f2f987
spbogui <spbogui@gmail.com>
2018-11-05 10:17:12 +0000

    Reporting form getter

---
+++ api/src/main/java/org/openmrs/module/ptme/forms/GetReportFromFrom.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.ptme.forms;
+
+public class GetReportFromFrom {
+    private String mode;
+
+    public GetReportFromFrom() {
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+}
